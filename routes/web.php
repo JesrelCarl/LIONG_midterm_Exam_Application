5a2d9994c93fc03864c5b609eb60c54fcb520351
JesrelCarl <153411912+JesrelCarl@users.noreply.github.com>
2025-02-22 04:18:37 +0000

    save files

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,6 @@
 use App\Http\Controllers\ProductController;
 
 Route::get('/products', [ProductController::class, 'index']);
+Route::middleware(['auth'])->get('/dashboard', function () {
+    return view('dashboard');
+});
