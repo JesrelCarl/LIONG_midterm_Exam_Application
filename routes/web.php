a8b39e55d4a5e10dcb7555ba0e2e4e7386136850
JesrelCarl <153411912+JesrelCarl@users.noreply.github.com>
2025-02-24 13:04:27 +0000

    save changes

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::get('/product', [ProductController::class, 'index']);
 
